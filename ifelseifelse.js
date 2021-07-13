5b873916a7730e0a8a3dd78a3324b50a078e76e4
muhammad-naufal-afif <86651475+muhammad-naufal-afif@users.noreply.github.com>
2021-07-13 20:55:27 +0700

    Add files via upload

---
+++ ifelseifelse.js
@@ -0,0 +1,12 @@
+let a = 70
+let b = 90
+let c = 50
+let d = 40
+
+if(b > a){
+    console.log('Terbesar : ' + b)
+}else if(c > a){
+    console.log('Terbesar : ' + c) 
+}else if(d > a){
+    console.log('Terbesar : ' + d)
+}
